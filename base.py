3e4ce37755469e5620f03a2f339523d0f79e69e1
Daxll <gilai87@gmail.com>
2020-05-21 15:21:44 +0300

    added platforms

--- base.py
+++ base.py
@@ -7,7 +7,17 @@ import os
 '''
 Objects
 '''
-
+class Platform(pygame.sprite.Sprite):
+# x location, y location, img width, img height, img file
+    def __init__(self,xloc,yloc,imgw,imgh,img):
+        ALPHA = (0, 0, 0)
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load(os.path.join('images',img)).convert()
+        self.image.convert_alpha()
+        self.image.set_colorkey(ALPHA)
+        self.rect = self.image.get_rect()
+        self.rect.y = yloc
+        self.rect.x = xloc
 
 class Player(pygame.sprite.Sprite):
     # Spawn a player
@@ -32,14 +42,21 @@ class Player(pygame.sprite.Sprite):
         # control player movement
 
         self.movex += x
-        self.movey += y
+        if self.rect.y == 500:
+            self.movey += y
+        else:
+            self.movey = 0
 
     def update(self):
 
         # Update sprite position
 
         self.rect.x = self.rect.x + self.movex
-        self.rect.y = self.rect.y + self.movey
+        if self.rect.y == (500 - steps*3):
+            self.rect.y = 500
+        else:
+            self.rect.y = self.rect.y + self.movey
+
         # moving left
         if self.movex < 0:
             self.frame += 1
@@ -53,6 +70,7 @@ class Player(pygame.sprite.Sprite):
             if self.frame > (3*ani-1):
                 self.frame = 0
             self.image = self.images[self.frame // 3]
+
 
         hit_list = pygame.sprite.spritecollide(self, enemy_list, False)
         for enemy in hit_list:
@@ -79,6 +97,30 @@ class Enemy(pygame.sprite.Sprite):
             self.rect.y = y
             self.counter = 0  # counter variable
 
+    def move(self):
+        '''
+        enemy movement
+        '''
+        distance = 300
+        speed = 2
+
+        if self.counter >= 0 and self.counter <= distance:
+            self.rect.x -= speed
+            self.frame += 1
+            if self.frame > (3 * ani - 1):
+                self.frame = 0
+            self.image = self.images[self.frame // 3]
+        elif self.counter >= distance and self.counter <= distance*2:
+            self.rect.x += speed
+            self.frame += 1
+            if self.frame > (3 * ani - 1):
+                self.frame = 0
+            self.image = self.images[self.frame // 3]
+        else:
+            self.counter = 0
+
+        self.counter += 1
+
 class Level():
     def bad(lvl, eloc, ename):
         if lvl == 1:
@@ -89,6 +131,29 @@ class Level():
             print("Level " + str(lvl) )
 
         return enemy_list
+
+    # def ground(lvl, x, y, w, h):
+    #     ground_list = pygame.sprite.Group()
+    #     if lvl == 1:
+    #         ground = Platform(x, y, w, h, 'block-ground.png')
+    #         ground_list.add(ground)
+    #
+    #     if lvl == 2:
+    #         print("Level " + str(lvl))
+    #
+    #     return ground_list
+
+    def platform(lvl):
+        plat_list = pygame.sprite.Group()
+        if lvl == 1:
+            plat = Platform(200, worldy - 97 - 128, 285, 67, 'plat1.png')
+            plat_list.add(plat)
+            plat = Platform(500, worldy - 97 - 320, 197, 54, 'plat1.png')
+            plat_list.add(plat)
+        if lvl == 2:
+            print("Level " + str(lvl))
+
+        return plat_list
 '''
 Setup
 '''
@@ -111,6 +176,8 @@ steps = 6  # how many pixels to move
 # enemy_list.add(enemy)                # add enemy to group
 
 enemy_list = Level.bad(1, [900,500], 'john')
+# ground_list = Level.ground( 1,0,worldy-97,1080,97 )
+plat_list   = Level.platform( 1 )
 
 BLUE = (25, 25, 200)
 BLACK = (23, 23, 23)
@@ -141,7 +208,7 @@ while main:
             if event.key == pygame.K_RIGHT or event.key == ord('d'):
                 player.control(steps, 0)
             if event.key == pygame.K_UP or event.key == ord('w'):
-                print('jump')
+                player.control(0, -steps*3)
 
         if event.type == pygame.KEYUP:
             if event.key == pygame.K_LEFT or event.key == ord('a'):
@@ -152,9 +219,14 @@ while main:
                 pygame.quit()
                 sys.exit()
                 main = False
+
     world.blit(backdrop, backdropbox)
     player.update()  # update player position
     player_list.draw(world)  # draw player
     enemy_list.draw(world)  # refresh enemies
+    for e in enemy_list:
+        e.move()
+    # ground_list.draw(world)  # refresh ground
+    plat_list.draw(world)  # refresh platforms
     pygame.display.flip()
     clock.tick(fps)
